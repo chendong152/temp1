70bea6517994e91206a1a2d9a0a9f93cdbc0c951
Dong Chen <techdong@hotmail.com>
2015-08-25 18:05:15 +0800

    终版（下）- 菜图片名换英文

--- biz/dishConfig.php
+++ biz/dishConfig.php
@@ -11,16 +11,16 @@ include_once __DIR__ . '/dish.php';
 $allDishes = array(
     //new Dish(3, 13, 3, 3, 3, '红烧千岛湖鱼头', "img/cai/红烧千岛湖鱼头.png"),
     //new Dish(11, 3, 4, 5, 6, '红烧长江小杂鱼', "img/cai/红烧长江小杂鱼.png"),
-    new Dish(3, 3, 13, 3, 3, '红透天龙虾', "img/cai/红透天龙虾.png"),
+    new Dish(3, 3, 13, 3, 3, '红透天龙虾', "img/cai/xia.png"),
     //new Dish(1, 13, 3, 13, 3, '花开富贵', "img/cai/花开富贵.png"),
    // new Dish(3, 3, 13, 3, 3, '黄河口四大缸', "img/cai/黄河口四大缸.png"),
-    new Dish(13, 1, 13, 8, 1, '李鸿章烩菜', "img/cai/李鸿章烩菜.png"),
-    new Dish(3, 3, 13, 13, 3, '全家福', "img/cai/全家福.png"),
+    new Dish(13, 1, 13, 8, 1, '李鸿章烩菜', "img/cai/li.png"),
+    new Dish(3, 3, 13, 13, 3, '全家福', "img/cai/quanjia.png"),
     //new Dish(13, 13, 3, 3, 3, '石锅酱焖牛肉', "img/cai/石锅酱焖牛肉.png"),
-    new Dish(3, 13, 3, 13, 3, '酸菜鱼', "img/cai/酸菜鱼.png"),
-    new Dish(13, 13, 3, 3, 3, '炭烤深海碟鱼头', "img/cai/炭烤深海碟鱼头.png"),
+    new Dish(3, 13, 3, 13, 3, '酸菜鱼', "img/cai/suancai.png"),
+    new Dish(13, 13, 3, 3, 3, '炭烤深海碟鱼头', "img/cai/yutou.png"),
    // new Dish(13, 3, 1, 3, 13, '鲜响螺冬瓜盅', "img/cai/鲜响螺冬瓜盅.png"),
-    new Dish(3, 13, 3, 3, 3, '湘楚霸王鸡', "img/cai/湘楚霸王鸡.png"),
+    new Dish(3, 13, 3, 3, 3, '湘楚霸王鸡', "img/cai/ji.png"),
 );
 /**
  * 根据菜品ID获取菜品实体
